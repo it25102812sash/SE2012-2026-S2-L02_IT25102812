2553448a347e66eaec0b3260c5204f26eb738b9f
it25102812sash <it25102812@my.sliit.lk>
2026-08-06 10:31:00 +0530

    enroll

--- StudentManagement/services/EnrollmentManager.java
+++ StudentManagement/services/EnrollmentManager.java
@@ -12,23 +12,52 @@ public class EnrollmentManager {
         studentCount = 0;
     }
 
+ 
     public void addStudent(String id, String name) {
+
         if (studentCount < students.length) {
+
             students[studentCount] = new Student(id, name);
             studentCount++;
 
             System.out.println("Student added successfully.");
+
         } else {
             System.out.println("Student list is full.");
         }
     }
 
+
+
     public void displayStudents() {
+
+        if (studentCount == 0) {
+            System.out.println("No students available.");
+            return;
+        }
+
         System.out.println("\n~~~ Student Details ~~~\n");
 
         for (int i = 0; i < studentCount; i++) {
             System.out.println(students[i]);
             System.out.println();
         }
+    }
+
+
+    
+    public void searchStudent(String id) {
+
+        for (int i = 0; i < studentCount; i++) {
+
+            if (students[i].getId().equals(id)) {
+
+                System.out.println("\nStudent Found:");
+                System.out.println(students[i]);
+                return;
+            }
+        }
+
+        System.out.println("Student not found.");
     }
 }
