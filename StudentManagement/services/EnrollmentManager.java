8ab7e5d2c6bb4cd60536c01367d17c512ef904ca
it25102812sash <it25102812@my.sliit.lk>
2026-08-06 10:08:27 +0530

    enroll

--- StudentManagement/services/EnrollmentManager.java
+++ StudentManagement/services/EnrollmentManager.java
@@ -1,48 +1,34 @@
 package services;
 
 import models.Student;
-import models.Course;
 
 public class EnrollmentManager {
-    private Student[] students;
-    private Course[] courses;
-    private int studentCount;
-    private int courseCount;
 
-    public EnrollmentManager(int maxStudents, int maxCourses) {
-        students = new Student[maxStudents];
-        courses = new Course[maxCourses];
+    private Student[] students;
+    private int studentCount;
+
+    public EnrollmentManager(int size) {
+        students = new Student[size];
         studentCount = 0;
-        courseCount = 0;
     }
 
     public void addStudent(String id, String name) {
         if (studentCount < students.length) {
             students[studentCount] = new Student(id, name);
             studentCount++;
+
+            System.out.println("Student added successfully.");
         } else {
             System.out.println("Student list is full.");
         }
     }
 
-    public void addCourse(String code, String title) {
-        if (courseCount < courses.length) {
-            courses[courseCount] = new Course(code, title);
-            courseCount++;
-        } else {
-            System.out.println("Course list is full.");
-        }
-    }
-
     public void displayStudents() {
+        System.out.println("\n~~~ Student Details ~~~\n");
+
         for (int i = 0; i < studentCount; i++) {
             System.out.println(students[i]);
-        }
-    }
-
-    public void displayCourses() {
-        for (int i = 0; i < courseCount; i++) {
-            System.out.println(courses[i]);
+            System.out.println();
         }
     }
 }
